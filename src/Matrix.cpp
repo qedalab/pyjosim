6cabe5661b16dda4edc67db41fde6ad6d8dfed41
Paul le Roux <pleroux0@outlook.com>
2020-02-10 15:39:24 +0200

    fix: Add missing file

---
+++ src/Matrix.cpp
@@ -0,0 +1,18 @@
+#include "JoSIM/Matrix.hpp"
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+namespace pyjosim {
+void matrix(py::module &m) {
+  using namespace JoSIM;
+
+  py::class_<Matrix>(m, "Matrix").def(py::init([](Input &input) {
+    Matrix matrix;
+    matrix.create_matrix(input);
+    RelevantTrace::find_relevant_traces(input.controls, matrix);
+    return matrix;
+  }));
+}
+
+} // namespace pyjosim
