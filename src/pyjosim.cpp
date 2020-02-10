275e0bc31a90b7b50a489d7859b71f28e9609f62
Paul le Roux <pleroux0@outlook.com>
2020-02-10 09:56:31 +0200

    feat: Implement CliOptions

--- src/pyjosim.cpp
+++ src/pyjosim.cpp
@@ -5,10 +5,16 @@ namespace py = pybind11;
 // Define submodules
 namespace pyjosim {
 
+void cli_options(py::module& m);
+
 }; // namespace pyjosim
 
 // Define python module
 PYBIND11_MODULE(pyjosim, m) {
+  using namespace pyjosim;
+
   m.doc() = "JoSIM python interface";
+
+  cli_options(m);
 }
 
