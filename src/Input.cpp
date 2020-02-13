116daae853038ad4a3a84e920ea9490630b2ba18
Paul le Roux <pleroux0@outlook.com>
2020-02-13 11:05:26 +0200

    feat: Implement more methods from Input as required by josim-tools

--- src/Input.cpp
+++ src/Input.cpp
@@ -20,12 +20,32 @@ void input(py::module &m)
         .def_readwrite("netlist", &Input::netlist)
         .def("parse_file",
              [](Input &self, std::string path) { self.parse_file(path); })
-        .def("parse_models", [](Input &self) {
-            for (const auto &model : self.netlist.models) {
-                auto pair = std::make_pair(model.second, model.first.second);
-                Model::parse_model(pair, self.netlist.models_new,
-                                   self.parameters);
-            }
+        .def("clone", [](const Input &input) -> Input { return input; })
+        .def("parse_models",
+             [](Input &self) {
+                 for (const auto &model : self.netlist.models) {
+                     auto pair =
+                         std::make_pair(model.second, model.first.second);
+                     Model::parse_model(pair, self.netlist.models_new,
+                                        self.parameters);
+                 }
+             })
+        .def("clear_all_plots",
+             [](Input &input) {
+                 input.controls.erase(std::remove_if(
+                     input.controls.begin(), input.controls.end(),
+                     [](const std::string &str) {
+                         for (const auto value : {"PLOT", "SAVE", "PRINT"}) {
+                             if (str.find(value) != std::string::npos)
+                                 return true;
+                         }
+
+                         return false;
+                     }));
+             })
+        .def("add_plot", [](Input &input, const std::string &str) {
+            input.controls.emplace_back(
+                std::move(std::string(".PRINT ").append(str)));
         });
 }
 
